d45e723c94e3172be3cee322522326f6387a3711
mo-alsafi <alsafimo12@gmail.com>
2024-11-28 21:57:16 +0200

    pep8

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 
-"""Contains make_change function."""
+""" Contains makeChange function"""
 
-def make_change(coins, total):
+
+def makeChange(coins, total):
     """
-    Returns: fewest number of coins needed to meet total.
-    
-    If total is 0 or less, return 0.
-    If total cannot be met by any number of coins you have, return -1.
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
     """
-    if not coins:
+    if not coins or coins is None:
         return -1
     if total <= 0:
         return 0
-    
     change = 0
-    coins = sorted(coins, reverse=True)
-    
+    coins = sorted(coins)[::-1]
     for coin in coins:
         while coin <= total:
             total -= coin
             change += 1
-        if total == 0:
+        if (total == 0):
             return change
-            
     return -1
